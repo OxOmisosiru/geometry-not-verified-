5bea6fef17d3310331a2802a4675ec5014383290
OxOmisosiru <63462098+OxOmisosiru@users.noreply.github.com>
2021-02-12 22:54:13 +0900

    Update Geometry.cpp

--- Geometry.cpp
+++ Geometry.cpp
@@ -1,18 +1,42 @@
-#include <algorithm>
-#include <utility> 
-#include <vector>
 #include <string>
+#include <algorithm>
 
 using namespace std;
 
 struct Point{
     long double x,y;
-
-    Point operator+(const Point &rhs){ return Point{x + rhs.x , y + rhs.y}; }
-    Point operator-(const Point &rhs){ return Point{x - rhs.x , y - rhs.y}; }
-    Point operator*(const long double scalar){return Point{x * scalar , y * scalar}; }
-    Point operator/(const long double scalar){return Point{x / scalar , y / scalar}; }
 };
+
+Point operator+(const Point &p , const Point &q){ 
+    Point ret;
+    ret.x = p.x + q.x; ret.y = p.y + q.y;
+    return ret;
+}
+Point operator-(const Point &p , const Point &q){ 
+    Point ret;
+    ret.x = p.x - q.x; ret.y = p.y - q.y;
+    return ret;
+}
+Point operator*(const Point &p , const Point &q){ 
+    Point ret;
+    ret.x = p.x * q.x; ret.y = p.y * q.y;
+    return ret;
+}
+Point operator*(const Point &p , const long double &a){ 
+    Point ret;
+    ret.x = p.x * a; ret.y = p.y * a;
+    return ret;
+}
+Point operator/(const Point &p , const Point &q){ 
+    Point ret;
+    ret.x = p.x / q.x; ret.y = p.y / q.y;
+    return ret;
+}
+Point operator/(const Point &p , const long double &a){ 
+    Point ret;
+    ret.x = p.x / a; ret.y = p.y / a;
+    return ret;
+}
 
 bool onSameLine_3points(Point p1, Point p2, Point p3){
     if((p1.x*p2.y + p2.x*p3.y + p3.x*p1.y) - (p1.x*p3.y + p2.x*p1.y + p3.x*p2.y) == 0){
@@ -37,8 +61,8 @@ long double dist_notsqrted(Point p1, Point p2){
 
 long double cross(Line l ,Line m){
     // l:Point a,b , m:Point c,d
-    Point a(l.p2 - l.p1);
-    Point b(m.p2 - m.p1);
+    Point a = l.p2 - l.p1;
+    Point b = m.p2 - m.p1;
     // return (by-ay)*(dx-cx) - (dy-cy)*(bx-ax)
     return abs(a.y * b.x - b.y * a.x);
 }
@@ -60,15 +84,18 @@ Point Centroid(Point p1, Point p2, Point p3){
 
 Point turn90(Point p){
     long double hozon = p.x;
-    p.x = -p.y; p.y = p.x;
+    p.x = -p.y; p.y = hozon;
     return p;
 }
 
 pair<Point,string> Intersection(Line l , Line m){
+    /*
+        THIS MAY CAUSE BUGS !
+    */
     // Intersection of l,m.
     Point intersection;
     long double d = cross(l,m);
-    long double EPS = 1e-12;
+    long double EPS = 1e-10;
     pair<Point,string> RETURN;
     if(d < EPS){
         // l,m are Parallel.
@@ -78,7 +105,8 @@ pair<Point,string> Intersection(Line l , Line m){
     }
     else{
         // y = ax+b
-        if(l.p2.x - l.p1.x == 0 || m.p2.x - m.p1.x == 0){
+        //cout << l.p2.x - l.p1.x << " " << d << endl;
+        if(abs(l.p2.x - l.p1.x) < EPS || abs(m.p2.x - m.p1.x) < EPS){
             // Div0
             RETURN.first = Point{0,0};
             RETURN.second = "FAIL";
@@ -87,7 +115,7 @@ pair<Point,string> Intersection(Line l , Line m){
         long double al = (l.p2.y - l.p1.y) / (l.p2.x - l.p1.x);
         long double am = (m.p2.y - m.p1.y) / (m.p2.x - m.p1.x);
         long double bunbo = al-am;
-        if(bunbo == 0){
+        if(abs(bunbo) < EPS){
             // Div0
             RETURN.first = Point{0,0};
             RETURN.second = "FAIL";
@@ -106,11 +134,12 @@ pair<Point,string> Intersection(Line l , Line m){
     }
 }
 
-pair<Point,string> CircumCenter(Point pa, Point pb, Point pc){
+Point CircumCenter(Point pa, Point pb, Point pc){
     Point cp;
     // pa,pb,pc aren't on the same Line.
-    Line ab((pa+pb)/2.0 , turn90(pa));
-    Line bc((pb+pc)/2.0 , turn90(pb));
-    pair<Point,string> cpstatus = Intersection(ab,bc);
-    return cpstatus;
+    // Reference HERE: https://en.wikipedia.org/wiki/Circumscribed_circle#Cartesian_coordinates
+    long double D = 2.0*(pa.x*(pb.y-pc.y) + pb.x*(pc.y-pa.y) + pc.x*(pa.y-pb.y));
+    cp.x = ((pa.x*pa.x + pa.y*pa.y)*(pb.y-pc.y) + (pb.x*pb.x + pb.y*pb.y)*(pc.y-pa.y) + (pc.x*pc.x + pc.y*pc.y)*(pa.y-pb.y)) / D;
+    cp.y = ((pa.x*pa.x + pa.y*pa.y)*(pc.x-pb.x) + (pb.x*pb.x + pb.y*pb.y)*(pa.x-pc.x) + (pc.x*pc.x + pc.y*pc.y)*(pb.x-pa.x)) / D;
+    return cp;
 }
